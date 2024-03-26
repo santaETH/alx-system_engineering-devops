b7b11c0830b6f51ea673d22e75e1cbb9d72c9f16
santaETH <sdmissie@yhoo.com>
2024-03-26 04:16:44 -0700

    configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-#!/usr/bin/pup
-#Install an especific version of flask 2.1.0
-package {'flask':
+# Install an especific version of flask 2.1.0
+package { 'puppet-lint':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => gem,
 }
