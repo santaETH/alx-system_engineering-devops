ed4a6992e22c32dca97e5c6211bd999e838c6565
santaETH <sdmissie@yhoo.com>
2024-03-26 01:45:08 -0700

    configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,18 +1,6 @@
 #!/usr/bin/pup
-# Using Puppet, install flask Version 2.1.0 from pip3.
-
-exec { 'uninstall_flask':
-  command => '/usr/bin/pip3 uninstall -y Flask',
-  onlyif  => '/usr/bin/pip3 show Flask',
-}
-
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'install_flask':
-  command     => '/usr/bin/pip3 install Flask==2.1.0',
-  path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  unless      => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
-  require     => Package['python3-pip'],
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
