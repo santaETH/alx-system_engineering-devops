5a2b5d6991c1b7381e377304931ab66f8872d101
santaETH <sdmissie@yhoo.com>
2024-03-27 08:26:44 -0700

    configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,10 +2,5 @@
 # Install an especific version of flask (2.1.0)
 package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip',
 }
-
-# Install werkzeug 2.1.1
-package { 'Werkzeug':
-  ensure => '2.1.1',
-  provider => 'pip3',
