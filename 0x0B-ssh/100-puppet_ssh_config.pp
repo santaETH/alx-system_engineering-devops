04c7b2aba0b78245e772b4d2a321084e33e7ad02
santaETH <sdmissie@yhoo.com>
2024-03-27 07:17:53 -0700

    ssh using puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Using Puppet to make changes to our configuration file.
+
+file { 'etc/ssh/ssh_config':
+	ensure => present,
+
+content =>"
+
+	#SSH client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+}
+
