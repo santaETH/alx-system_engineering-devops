9dcd2b536d9bab6f74923c50c9c28d848380918f
santaETH <sdmissie@yhoo.com>
2024-03-28 00:10:45 -0700

    CM

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,5 @@
+#!/usr/bin/pip
 # Install an especific version of flask (2.1.0)
 package {'python3-pip':
   ensure   => installed,
-}
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.0.0',
-  path => ['/usr/bin'],
-  unless => '/usr/bin/flask --version | grep "2.1.0"',
 }
