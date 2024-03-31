4a5c5cde75a15bfd02866fa9c4357994313cab93
santaETH <sdmissie@yhoo.com>
2024-03-31 07:13:34 -0700

    web server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,11 +4,11 @@ package { 'nginx':
   ensure => installed,
 }
 
-file_line { 'install':
+file_line { 'redirection-301':
   ensure => 'present',
-  path	 => '/etc/nginx/sites-enabled/default',
+  path	 => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 file { '/var/www/html/index.nginx-debian.html:
