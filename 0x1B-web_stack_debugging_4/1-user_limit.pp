afd5fcd3f58c73d4fd28e70ad4e4f913bc9fba50
santaETH <sdamissie@yahoo.com>
2024-06-15 01:23:29 -0700

    web_stack_debugging_4

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,6 @@
+# Change the OS configuration for 'holberton' to login and open a file without any error message
+
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
